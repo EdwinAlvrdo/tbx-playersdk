634f5091581f4e03e52105cf5cdf21ea9a4acbf4
Edwin Alvarado <ealvarado@toolboxtve.com>
2021-03-30 13:21:41 -0400

    Initial Commit

---
+++ TBXPlayerSDK/TBXPlayerSDK.h
@@ -0,0 +1,18 @@
+//
+//  TBXPlayerSDK.h
+//  TBXPlayerSDK
+//
+//  Created by Edwin Alvarado on 3/30/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TBXPlayerSDK.
+FOUNDATION_EXPORT double TBXPlayerSDKVersionNumber;
+
+//! Project version string for TBXPlayerSDK.
+FOUNDATION_EXPORT const unsigned char TBXPlayerSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TBXPlayerSDK/PublicHeader.h>
+
+
